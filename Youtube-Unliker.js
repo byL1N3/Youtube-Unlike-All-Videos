1c19b23ad19d0d348bba3a3e68bc0a0737713b2d
byL1N3 <77385729+byL1N3@users.noreply.github.com>
2021-10-24 15:09:01 +0300

    Add files via upload

---
+++ Youtube-Unliker.js
@@ -0,0 +1,21 @@
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function deleteLikedVideos() {
+    'use strict';
+    var items = document.querySelectorAll('#primary ytd-playlist-video-renderer yt-icon-button.dropdown-trigger > button[aria-label]');
+    var out;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].click();
+        out = setTimeout(function () {
+            if (document.querySelector('tp-yt-paper-listbox.style-scope.ytd-menu-popup-renderer').lastElementChild) {
+                document.querySelector('tp-yt-paper-listbox.style-scope.ytd-menu-popup-renderer').lastElementChild.click();
+            }
+        }, 100);
+        await sleep(500); // sleep cause browser can not handle the process
+        clearTimeout(out);
+    }
+}
+
+deleteLikedVideos();
