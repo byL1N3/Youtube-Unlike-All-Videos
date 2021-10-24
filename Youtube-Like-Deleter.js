2180d4cc2fe19e0865f344cc62fa598aa10dc8ed
byL1N3 <77385729+byL1N3@users.noreply.github.com>
2021-10-24 15:18:38 +0300

    Update Youtube-Like-Deleter.js

--- Youtube-Like-Deleter.js
+++ Youtube-Like-Deleter.js
@@ -1,21 +1,19 @@
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function deleteLikedVideos() {
-    'use strict';
-    var items = document.querySelectorAll('#primary ytd-playlist-video-renderer yt-icon-button.dropdown-trigger > button[aria-label]');
-    var out;
-
-    for (var i = 0; i < items.length; i++) {
-        items[i].click();
-        out = setTimeout(function () {
-            if (document.querySelector('tp-yt-paper-listbox.style-scope.ytd-menu-popup-renderer').lastElementChild) {
-                document.querySelector('tp-yt-paper-listbox.style-scope.ytd-menu-popup-renderer').lastElementChild.click();
-            }
-        }, 100);
-        await sleep(500); // sleep cause browser can not handle the process
-        clearTimeout(out);
-    }
-}
-
-deleteLikedVideos();
+function sleep(ms) { 
+return new Promise(resolve => setTimeout(resolve, ms)); 
+} 
+async function deleteLikedVideos() { 
+'use strict'; 
+var items = document.querySelectorAll('ytd-menu-renderer > yt-icon-button.dropdown-trigger > button[aria-label]'); 
+var out; 
+for (var i = 0; i < items.length; i++) { 
+items[i].click(); 
+out = setTimeout(function () { 
+if (document.querySelector('tp-yt-paper-listbox.style-scope.ytd-menu-popup-renderer').lastElementChild) { 
+document.querySelector('tp-yt-paper-listbox.style-scope.ytd-menu-popup-renderer').lastElementChild.click(); 
+} 
+}, 100); 
+await sleep(500); // sleep cause browser can not handle the process 
+clearTimeout(out); 
+} 
+} 
+deleteLikedVideos(); 
